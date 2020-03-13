48b5090fcac9e7d3588d369d45e33c912aa59210
Lauren Hallau <lhallau@gmail.com>
2020-03-13 13:31:26 -0500

    seed and schema files have been successfully uploaded to mysql

--- schema.sql
+++ schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_title VARCHAR (30) NOT NULL,
     role_salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
